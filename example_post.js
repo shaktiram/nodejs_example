785f0fec368815742ed06883366fb1f46a3d26b1
shaktiram <shakthi16277@gmail.com>
2017-01-23 11:45:31 +0530

    first

---
+++ example_post.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/', function(req,res){
+  res.send(req.body);
+});
+
+app.get('/:id', function(req,res){
+  res.send(req.params.id);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
